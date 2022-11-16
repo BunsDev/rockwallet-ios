1033b91cfdbbea4e285a8f2e0c338b6bb4d6c568
rcresnik <rok.cresnik@equaleyes.com>
2022-11-16 13:21:37 +0100

    fix: form validation.

--- brd-ios/breadwallet/src/Redesign/Scenes/KYC/KYCLevelOne/KYCBasicStore.swift
+++ brd-ios/breadwallet/src/Redesign/Scenes/KYC/KYCLevelOne/KYCBasicStore.swift
@@ -22,5 +22,9 @@ class KYCBasicStore: NSObject, BaseDataStore, KYCBasicDataStore {
     var birthDateString: String?
     
     var countries: [Country]?
+    
+    var needsState: Bool {
+        return country == "US"
+    }
     // MARK: - Aditional helpers
 }
