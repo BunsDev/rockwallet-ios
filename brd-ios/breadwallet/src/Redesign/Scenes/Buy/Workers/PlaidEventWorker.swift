17503b1264f07e2c9f02319844153c23d4eee5e1
rcresnik <rok.cresnik@equaleyes.com>
2022-12-06 11:04:42 +0100

    feat: add plaid event/error workers.

---
+++ brd-ios/breadwallet/src/Redesign/Scenes/Buy/Workers/PlaidEventWorker.swift
@@ -0,0 +1,35 @@
+// 
+//  PlaidEventWorker.swift
+//  breadwallet
+//
+//  Created by Rok on 06/12/2022.
+//  Copyright © 2022 RockWallet, LLC. All rights reserved.
+//
+//  See the LICENSE file at the project root for license information.
+//
+
+import Foundation
+
+struct PlaidEventRequestData: RequestModelData {
+    let event: String
+    
+    func getParameters() -> [String: Any] {
+        return [
+            "plaid_callback_event": event
+        ]
+    }
+}
+
+class PlaidEventWorker: BaseApiWorker<PlainMapper> {
+    override func getUrl() -> String {
+        return ExchangeEndpoints.plaidLogEvent.url
+    }
+
+    override func getParameters() -> [String: Any] {
+        return requestData?.getParameters() ?? [:]
+    }
+
+    override func getMethod() -> HTTPMethod {
+        return .post
+    }
+}
