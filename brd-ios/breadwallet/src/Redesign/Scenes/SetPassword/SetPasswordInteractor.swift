3b682eedf53c9d98410a76d3592f2791c42792ef
Kenan Mamedoff <kenan.mamedoff@equaleyes.com>
2023-01-12 16:56:16 +0700

    feat: set password screen

---
+++ brd-ios/breadwallet/src/Redesign/Scenes/SetPassword/SetPasswordInteractor.swift
@@ -0,0 +1,42 @@
+//
+//  SetPasswordInteractor.swift
+//  breadwallet
+//
+//  Created by Kenan Mamedoff on 11/01/2022.
+//  Copyright © 2023 RockWallet, LLC. All rights reserved.
+//
+//  See the LICENSE file at the project root for license information.
+//
+
+import UIKit
+
+class SetPasswordInteractor: NSObject, Interactor, SetPasswordViewActions {
+    
+    typealias Models = SetPasswordModels
+    
+    var presenter: SetPasswordPresenter?
+    var dataStore: SetPasswordStore?
+    
+    // MARK: - SetPasswordViewActions
+    
+    func getData(viewAction: FetchModels.Get.ViewAction) {
+        presenter?.presentData(actionResponse: .init(item: Models.Item(password: "", passwordAgain: "")))
+    }
+    
+    func validate(viewAction: SetPasswordModels.Validate.ViewAction) {
+        if let password = viewAction.password {
+            dataStore?.password = password
+        }
+        
+        if let passwordAgain = viewAction.passwordAgain {
+            dataStore?.passwordAgain = passwordAgain
+        }
+        
+        let isPasswordValid = FieldValidator.validate(fields: [dataStore?.password,
+                                                               dataStore?.passwordAgain]) && dataStore?.password == dataStore?.passwordAgain
+        
+        presenter?.presentValidate(actionResponse: .init(isValid: isPasswordValid))
+    }
+    
+    // MARK: - Aditional helpers
+}
