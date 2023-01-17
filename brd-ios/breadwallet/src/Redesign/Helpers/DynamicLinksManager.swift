073f489cf04dd8cbcaab8b4fadb73d45502f46dc
Kenan Mamedoff <kenan.mamedoff@equaleyes.com>
2023-01-17 18:20:42 +0800

    chore: cleanup

--- brd-ios/breadwallet/src/Redesign/Helpers/DynamicLinksManager.swift
+++ brd-ios/breadwallet/src/Redesign/Helpers/DynamicLinksManager.swift
@@ -41,7 +41,7 @@ class DynamicLinksManager {
     }
     
     private static func handleReSetPassword(on coordinator: BaseCoordinator?, dynamicLinkType: DynamicLinkType, with url: URL) {
-        guard let parameters = url.getQueryParameters(),
+        guard let parameters = url.queryParameters,
               let code = parameters["code"],
               let guid = parameters["guid"] else {
             return
@@ -51,25 +51,5 @@ class DynamicLinksManager {
             vc?.dataStore?.code = code
             vc?.dataStore?.guid = guid
         }
-    }
-}
-
-extension URL {
-    func getQueryParameters() -> [String: String]? {
-        var results = [String: String]()
-        
-        guard let keyValues = query?.components(separatedBy: "&") else { return nil }
-        
-        if keyValues.isEmpty == false {
-            for pair in keyValues {
-                let kv = pair.components(separatedBy: "=")
-                
-                if kv.count > 1 {
-                    results.updateValue(kv[1].removingPercentEncoding ?? kv[1], forKey: kv[0])
-                }
-            }
-        }
-        
-        return results
     }
 }
