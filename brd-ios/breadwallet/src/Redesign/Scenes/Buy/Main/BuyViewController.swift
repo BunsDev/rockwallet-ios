1cf4e1a929973a40e8c1d9c13cbdbc5d378c6958
Dijana <angelovska.dijana@hotmail.com>
2023-01-19 23:20:32 +0100

    fix: update segment control when retry payment with ach

--- brd-ios/breadwallet/src/Redesign/Scenes/Buy/Main/BuyViewController.swift
+++ brd-ios/breadwallet/src/Redesign/Scenes/Buy/Main/BuyViewController.swift
@@ -113,15 +113,14 @@ class BuyViewController: BaseTableViewController<BuyCoordinator, BuyInteractor, 
     
     override func tableView(_ tableView: UITableView, segmentControlCellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let section = sections[indexPath.section]
-        guard let cell: WrapperTableViewCell<FESegmentControl> = tableView.dequeueReusableCell(for: indexPath),
-              let model = sectionRows[section]?[indexPath.row] as? SegmentControlViewModel
+        guard let cell: WrapperTableViewCell<FESegmentControl> = tableView.dequeueReusableCell(for: indexPath)
         else {
             return UITableViewCell()
         }
         
         cell.setup { view in
             view.configure(with: .init())
-            view.setup(with: model)
+            view.setup(with: SegmentControlViewModel(selectedIndex: dataStore?.paymentMethod))
             
             view.didChangeValue = { [weak self] segment in
                 self?.view.endEditing(true)
@@ -285,6 +284,8 @@ class BuyViewController: BaseTableViewController<BuyCoordinator, BuyInteractor, 
         guard let availablePayments = dataStore?.availablePayments else { return }
         
         dataStore?.paymentMethod = availablePayments.contains(.ach) == true ? .ach : .card
+        tableView.reloadData()
+        
         interactor?.retryPaymentMethod(viewAction: .init(method: dataStore?.paymentMethod ?? .card))
     }
     
