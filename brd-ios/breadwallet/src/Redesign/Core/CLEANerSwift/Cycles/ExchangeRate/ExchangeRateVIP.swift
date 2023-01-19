70fcddcbc297144f84accdb0f76f024150410a1c
rcresnik <rok.cresnik@equaleyes.com>
2023-01-19 13:53:42 +0100

    fix: inverse rate.

--- brd-ios/breadwallet/src/Redesign/Core/CLEANerSwift/Cycles/ExchangeRate/ExchangeRateVIP.swift
+++ brd-ios/breadwallet/src/Redesign/Core/CLEANerSwift/Cycles/ExchangeRate/ExchangeRateVIP.swift
@@ -86,7 +86,7 @@ extension Presenter where Self: ExchangeRateActionResponses,
                let to = actionResponse.to,
                let quote = actionResponse.quote,
                let showTimer = actionResponse.showTimer {
-                let text = String(format: "1 %@ = %@ %@", from.uppercased(), RWFormatter().string(for: 1 / quote.exchangeRate) ?? "", to)
+                let text = String(format: "1 %@ = %@ %@", from.uppercased(), RWFormatter().string(for: quote.exchangeRate) ?? "", to)
 
                 exchangeRateViewModel = ExchangeRateViewModel(exchangeRate: text,
                                                               timer: TimerViewModel(till: quote.timestamp, repeats: false),
