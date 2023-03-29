d2a8c2ce72ac93fee2d13a68d4bed6784f525848
Kanan Mamedoff <k.mamedoff@nchain.com>
2023-03-29 10:01:06 +0200

    fix: sardine

---
+++ brd-ios/breadwallet/Helpers/Networking/Endpoints/SardineEndpoints.swift
@@ -0,0 +1,21 @@
+// 
+//  SardineEndpoints.swift
+//  breadwallet
+//
+//  Created by Kanan Mamedoff on 28/03/2023.
+//  Copyright © 2023 RockWallet, LLC. All rights reserved.
+//
+//  See the LICENSE file at the project root for license information.
+//
+
+import Foundation
+
+enum SardineEndpoints: String, URLType {
+    static var baseURL: String = "https://" + E.apiUrl + "blocksatoshi/one/sardine/%@"
+    
+    case session = "session"
+    
+    var url: String {
+        return String(format: Self.baseURL, rawValue)
+    }
+}
