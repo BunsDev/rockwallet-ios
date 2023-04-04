284bfe38dac335f21c5ddb8a4493d555a5bb0669
Dino Gacevic <dino.gacevic@equaleyes.com>
2023-04-04 09:07:42 +0200

    refactor: asset details footer view

---
+++ brd-ios/breadwallet/src/Redesign/Views/RWBottomBarItem.swift
@@ -0,0 +1,96 @@
+// 
+//  RWBottomBarItem.swift
+//  breadwallet
+//
+//  Created by Dino Gacevic on 03/04/2023.
+//  Copyright © 2023 RockWallet, LLC. All rights reserved.
+//
+//  See the LICENSE file at the project root for license information.
+//
+
+import UIKit
+
+struct RWBottomBarItemConfiguration: Configurable {
+}
+
+struct RWBottomBarItemViewModel: ViewModel {
+    var title: String
+    var image: UIImage?
+    var callback: (() -> Void)?
+}
+
+class RWBottomBarItem: FEView<RWBottomBarItemConfiguration, RWBottomBarItemViewModel> {
+    static let defaultheight: CGFloat = 72.0
+    
+    private lazy var imageContainerView: UIView = {
+        let container = UIView()
+        container.clipsToBounds = true
+        container.backgroundColor = LightColors.primary
+        return container
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: Asset.buy.image)
+        imageView.tintColor = LightColors.Contrast.two
+        return imageView
+    }()
+    
+    private lazy var label: FELabel = {
+        let label = FELabel()
+        label.textAlignment = .center
+        label.configure(with: .init(font: Fonts.Body.three, textColor: LightColors.Text.one))
+        return label
+    }()
+    
+    private lazy var tappableView: UIView = {
+        return UIView()
+    }()
+    
+    private var onTap: (() -> Void)?
+    
+    override func setupSubviews() {
+        super.setupSubviews()
+        
+        content.addSubview(imageContainerView)
+        
+        imageContainerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.width.equalTo(ViewSizes.Common.defaultCommon.rawValue)
+        }
+        imageContainerView.layer.cornerRadius = CornerRadius.fullRadius.rawValue  * ViewSizes.Common.defaultCommon.rawValue
+        
+        imageContainerView.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(Margins.medium.rawValue)
+        }
+        
+        content.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.top.equalTo(imageContainerView.snp.bottom).offset(Margins.small.rawValue)
+            make.centerX.equalTo(imageContainerView.snp.centerX)
+        }
+        
+        content.addSubview(tappableView)
+        tappableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        tappableView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped(_:))))
+    }
+    
+    override func configure(with config: RWBottomBarItemConfiguration?) {
+        super.configure(with: config)
+    }
+    
+    override func setup(with viewModel: RWBottomBarItemViewModel?) {
+        guard let viewModel = viewModel else { return }
+        
+        imageView.image = viewModel.image
+        label.setup(with: .text(viewModel.title))
+        onTap = viewModel.callback
+    }
+    
+    @objc func tapped(_ sender: UITapGestureRecognizer) {
+        onTap?()
+    }
+}
