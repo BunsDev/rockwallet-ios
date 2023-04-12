156379b3277b72d007081b095d526e4fce619d27
Kanan Mamedoff <k.mamedoff@nchain.com>
2023-04-12 11:27:45 +0200

    chore: cleanup

--- brd-ios/breadwallet/src/Redesign/Views/Core/FESegmentControl.swift
+++ brd-ios/breadwallet/src/Redesign/Views/Core/FESegmentControl.swift
@@ -115,33 +115,3 @@ class FESegmentControl: UISegmentedControl, ViewProtocol {
         }
     }
 }
-
-extension UIImage {
-    static func textEmbeded(image: UIImage,
-                            string: String,
-                            isImageBeforeText: Bool,
-                            font: UIFont = Fonts.button) -> UIImage {
-        let expectedTextSize = (string as NSString).size(withAttributes: [.font: font])
-        let width = expectedTextSize.width + image.size.width + 5
-        let height = max(expectedTextSize.height, image.size.width)
-        let size = CGSize(width: width, height: height)
-
-        let renderer = UIGraphicsImageRenderer(size: size)
-        return renderer.image { _ in
-            let fontTopPosition: CGFloat = (height - expectedTextSize.height) / 2
-            let textOrigin: CGFloat = isImageBeforeText
-                ? image.size.width + 5
-                : 0
-            let textPoint: CGPoint = CGPoint.init(x: textOrigin, y: fontTopPosition)
-            string.draw(at: textPoint, withAttributes: [.font: font])
-            let alignment: CGFloat = isImageBeforeText
-                ? 0
-                : expectedTextSize.width + 5
-            let rect = CGRect(x: alignment,
-                              y: (height - image.size.height) / 2,
-                          width: image.size.width,
-                         height: image.size.height)
-            image.draw(in: rect)
-        }
-    }
-}
