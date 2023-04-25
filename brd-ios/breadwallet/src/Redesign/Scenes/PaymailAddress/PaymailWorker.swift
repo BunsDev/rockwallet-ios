b0758a105d25d734e493c64389f11f38dc80e3e8
Dijana <angelovska.dijana@hotmail.com>
2023-04-25 14:45:34 +0200

    chore: add paymail worker

---
+++ brd-ios/breadwallet/src/Redesign/Scenes/PaymailAddress/PaymailWorker.swift
@@ -0,0 +1,38 @@
+// 
+//  PaymailWorker.swift
+//  breadwallet
+//
+//  Created by Dijana Angelovska on 25.4.23.
+//  Copyright © 2023 RockWallet, LLC. All rights reserved.
+//
+//  See the LICENSE file at the project root for license information.
+//
+
+import Foundation
+
+struct PaymailRequestData: RequestModelData {
+    let paymail: String?
+    let xpub: String?
+    
+    func getParameters() -> [String: Any] {
+        let params = [
+            "paymail": paymail,
+            "xpub": xpub
+        ]
+        return params.compactMapValues { $0 }
+    }
+}
+
+class PaymailWorker: BaseApiWorker<PlainMapper> {
+    override func getUrl() -> String {
+        return PaymailEndpoints.paymail.url
+    }
+
+    override func getParameters() -> [String: Any] {
+        return requestData?.getParameters() ?? [:]
+    }
+
+    override func getMethod() -> HTTPMethod {
+        return .post
+    }
+}
