68b951f63a323864b17e48d304500f35c561a235
Kanan Mamedoff <k.mamedoff@nchain.com>
2023-05-09 09:38:51 +0200

    chore: ConfirmationCodesWorker

--- brd-ios/breadwallet/src/Redesign/Scenes/VerifyPhoneNumber/Auth/ConfirmationCodesWorker.swift
+++ brd-ios/breadwallet/src/Redesign/Scenes/VerifyPhoneNumber/Auth/ConfirmationCodesWorker.swift
@@ -1,11 +1,18 @@
 // 
-//  File.swift
+//  ConfirmationCodesWorker.swift
 //  breadwallet
 //
-//  Created by Kanan Mamedoff on 09/05/2023.
+//  Created by Kenan Mamedoff on 09/05/2023.
 //  Copyright © 2023 RockWallet, LLC. All rights reserved.
 //
 //  See the LICENSE file at the project root for license information.
 //
 
 import Foundation
+
+class ConfirmationCodesWorker: BaseApiWorker<PlainMapper> {
+    override func getUrl() -> String {
+        guard E.isDevelopment else { return "" }
+        return KYCAuthEndpoints.confirmationCodes.url
+    }
+}
