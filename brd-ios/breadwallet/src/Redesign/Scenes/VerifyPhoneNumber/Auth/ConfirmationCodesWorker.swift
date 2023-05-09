36548988cb5bb1e209d5ea649c87926eee4a307c
Kanan Mamedoff <k.mamedoff@nchain.com>
2023-05-09 09:38:27 +0200

    fix: prompt crash

---
+++ brd-ios/breadwallet/src/Redesign/Scenes/VerifyPhoneNumber/Auth/ConfirmationCodesWorker.swift
@@ -0,0 +1,11 @@
+// 
+//  File.swift
+//  breadwallet
+//
+//  Created by Kanan Mamedoff on 09/05/2023.
+//  Copyright © 2023 RockWallet, LLC. All rights reserved.
+//
+//  See the LICENSE file at the project root for license information.
+//
+
+import Foundation
