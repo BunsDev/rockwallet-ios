809e61012cf33c8a8ab1bb5ba925c8aa8006fb57
Kanan Mamedoff <k.mamedoff@nchain.com>
2023-05-24 11:28:31 +0400

    chore: rename param

--- brd-ios/breadwallet/src/Redesign/Scenes/Profile/ProfileViewController.swift
+++ brd-ios/breadwallet/src/Redesign/Scenes/Profile/ProfileViewController.swift
@@ -110,7 +110,7 @@ class ProfileViewController: BaseTableViewController<ProfileCoordinator,
                         self?.coordinator?.showFailure(reason: .documentVerificationRetry)
                         
                     default:
-                        self?.coordinator?.showKYCLevelOne(isModally: true)
+                        self?.coordinator?.showKYCLevelOne(isModal: true)
                     }
                 }
             }
